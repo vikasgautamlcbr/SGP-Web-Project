362f20b5a66098e00f6659925845adb6dbdd5b9a
vikasgautamlcbr <vikasgautamlcbr@gmail.com>
2026-04-02 17:14:55 +0530

    update

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.cache = false;
+    }
+    return config;
+  }
+};
+
+module.exports = nextConfig;
